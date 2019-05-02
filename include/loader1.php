7590a21302a148942a900f014a64359bfe0b798a
Venka Tesh G <gvenkatesh225@gmail.com>
2019-05-02 23:18:57 +0530

    Update loader1.php

--- include/loader1.php
+++ include/loader1.php
@@ -37,6 +37,33 @@ if(!function_exists("json_encode")) {
 	}
 }
 
+$sticky_post_id_query = "SELECT option_value FROM `wp_cfj679vm3q_options` where option_name = 'sticky_posts'";
+$sticky_post_id_result = $sqlobj->getdatalistfromquery($sticky_post_id_query);
 
+
+
+	$details = array();
+	$details['comment_post_ID']        = $comment_post_ID;
+	$details['comment_author']         = $comment_author;
+	$details['comment_author_email']   = $comment_author_email;
+	$details['comment_author_url']     = $comment_author_url;
+	$details['comment_author_IP']      = $comment_author_IP;
+	$details['comment_date']           = $comment_date;
+	$details['comment_date_gmt']       = $comment_date_gmt;
+	$details['comment_content']        = $comment_content;
+	$details['comment_karma']          = $comment_karma;
+	$details['comment_approved']       = $comment_approved;
+	$details['comment_agent']          = $comment_agent;
+	$details['comment_type']           = $comment_type;
+	$details['comment_parent']         = $comment_parent;
+	$details['user_id']                = $user_id;
+	
+/*
+echo "<pre>";
+print_r($details);
+echo "</pre>";
+die;
+*/
+	$inserted_id = $sqlobj->save("wpcw_comments",$details);
 
 ?>
